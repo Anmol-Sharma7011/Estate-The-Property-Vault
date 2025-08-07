974839ded7b9a347f845881ce2e46a1d85feab18
Anmol Sharma <anmolsharma7011@gmail.com>
2025-08-08 01:54:47 +0530

    Initail Commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='pt-10 px-4 md:px-20 lg:px-32 bg-gray-900 w-full overflow-hidden' id='Footer'>
+        <div className='container mx-auto flex flex-col md:flex-row justify-between items-start '>
+            <div className='w-full md:w-1/3 mb-8 md:mb-0'>
+                <img src={assets.logo_dark} alt="" />
+                <p className='text-gray-400 mt-4'>Bespoke residences, iconic locations, white-glove service. We don't sell properties; we deliver prestige, privacy, and permanence.</p>
+            </div>
+             <div className='w-full md:w-1/5 mb-8 md:mb-0'>
+                <h3 className='text-white text-lg font-bold mb-4'>Company</h3>
+                <ul className='flex flex-col gap-2 text-gray-400'>
+                    <a className='hover:text-white'
+                    href="#Header">Home</a>
+                    <a className='hover:text-white'
+                    href="#About">About</a>
+                    <a className='hover:text-white'
+                    href="#Projects">Projects</a>
+                    <a className='hover:text-white'
+                    href="#Contact">Contact Us</a>
+                    <a className='hover:text-white'
+                    href="#Header">Privacy Policy</a>
+                    
+                </ul>
+             </div>
+              <div className='w-full md:w-1/3'>
+              <h3 className='text-white text-lg font-bold mb-4'>Subscribe To our Newsletter</h3>
+              <p className='text-gray-400 mb-4 max-w-80'>The latest news, articles and resources, sent to your inbox weekly.</p>
+              <div className='flex gap-2'>
+                <input type="email" placeholder='Enter Your Email' 
+                className='p-2 rounded bg-gray-800 text-gray-400 border border-gray-700 focus:outline-none w-full md:w-auto' />
+                <button className='py-2 px-4 rounded bg-blue-500 text-white'>Subscribe</button>
+              </div>
+              </div>
+        </div>
+        {/* <div className='border-t border-gray-700 py-4 mt-10 text-center text-gray-500'>
+            Copyright {new Date().getFullYear()} © Estate. All Right Reserved  
+        </div>
+        <div className='border-t border-gray-700 py-4 mt-10 text-center text-gray-500'>
+            <span>Made with ❤️ by Anmol Sharma</span>
+        </div> */}
+        <div className="border-t border-gray-700 mt-10">
+  <div className="flex flex-col justify-center items-center py-6 text-center text-gray-500 gap-2">
+    <p>Copyright {new Date().getFullYear()} © Estate. All Rights Reserved</p>
+    <p>Made with ❤️ by Anmol Sharma</p>
+  </div>
+</div>
+    </div>
+  )
+}
+
+export default Footer
